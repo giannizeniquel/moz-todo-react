577dbb2a6a27638cab0ed70d9d7b7141700a4727
zeniquelgianniivan <zeniquelgianniivan@chaco.gov.ar>
2022-12-13 15:25:38 -0300

    podemos agregar tareas, uso de useState, fix contador y label de tareas, eventos, devoluciones de llamadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid"; //libreria externa para generar ids
+
+
 
 function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name){
+    const newTask = {id: `todo-${nanoid()}`, name: name, completed: false};
+    setTasks([...tasks, newTask]); // asi copio la matriz original y agrego mi nuevo elemento a lo ultimo
+  }
+
   //recorro matriz de tareas que fue declarada en Index.js y recibo en props como tasks
-  const taskList = props.tasks.map((task) => ( 
+  const taskList = tasks.map((task) => ( 
     <Todo 
       id={task.id} 
       name={task.name} 
@@ -12,20 +24,23 @@ function App(props) {
       key={task.id}/>
   ));
 
+  const tasksNoun = taskList.length !== 1 ? 'tareas' : 'tarea';
+  const headingText = `${taskList.length} ${tasksNoun}`;
+
   return (
     <div className="todoapp stack-large">
       <h1>Administrar Tareas</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton name="Todos" pressed="true" />
         <FilterButton name="Activas" />
         <FilterButton name="Completadas"/> 
       </div>
       <h2 id="list-heading">
-        Quedan {taskList.length} tareas
+        {headingText}
       </h2>
       <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading" >
-        {/* muestro constante que contiene mi mapeo con mis Todos */}
+        {/* muestro constante que contiene mi mapeo con mis tareas */}
         {taskList}
       </ul>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+
+
 
 export default function Form (props) {
+
+    const [name, setName] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+        props.addTask(name);
+        setName('');
+    }
+
+    function handleChange(e){
+        setName(e.target.value);
+    }
+
     return (
         <Fragment>
-            <form>
+            <form >
                 <h2 className="label-wrapper">
                 <label htmlFor="new-todo-input" className="label__lg">
                     Que tarea tiene pendiente?
@@ -15,8 +30,10 @@ export default function Form (props) {
                     className="input input__lg"
                     name="text"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleChange}
                 />
-                <button type="submit" className="btn btn__primary btn__lg">
+                <button type="submit" className="btn btn__primary btn__lg" onClick={handleSubmit}>
                     Agregar
                 </button>
             </form>
